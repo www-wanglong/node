35153d47cac5b300c9c7ba1f1c7ae89bbdccc2dc
wanglong <18392700041@163.com>
2020-05-01 14:11:57 +0800

    test

--- hello1.js
+++ hello1.js
@@ -1,21 +1,4 @@
-//node的js的实现原理
-var module = {
-  id: 'hello',
-  exports: {
-
-  }
-}
-
-var load = function (module) {
-  function greet(name) {
-    console.log('Hello,' + name + '!')
-  }
-
-  module.exports = greet
-
-  return module.exports
-}
-
-var exported = load(module)
-
-save(module, exported)
+var Hello = require('./hello')
+hello = new Hello()
+hello.setName('wang')
+hello.sayHello()
